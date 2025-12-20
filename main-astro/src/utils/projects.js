a50b2e3974bce286779a1159c3ff3b5209e8789d
Will Domine <willdomine1@outlook.com>
2025-12-20 09:59:52 -0800

    updated project information and webpage-title

--- main-astro/src/utils/projects.js
+++ main-astro/src/utils/projects.js
@@ -1,8 +1,8 @@
 const projects = [
-    {title: "Personal Website", description:"A workspace for displaying my projects, skills, and experience to the world!",image:"/personal_website.png", github_link:"https://localhost:4173", website_link: "https://loaclhost:4173", tech: ["Astro","Tailwind","Render"], featured:true, completed:true },
-    {title: "Aurelius", description:"A Vulkan game engine, created for optimization.",image:"", github_link:"https://localhost:4173", website_link: "https://loaclhost:4173", tech: ["C++","Vulkan API"], featured:false, completed:false },
-    {title: "Scope of Employment", description:"Legal Chatbot, utilizing RAG to feed context to LLM",image:"", github_link:"https://localhost:4173", website_link: "https://loaclhost:4173", tech: ["Ollama","Qdrant","FastAPI","Flask"], featured:true, completed:true },
-    {title: "HoopHub", description:"NBA statistics tracker",image:"", github_link:"https://localhost:4173", website_link: "https://loaclhost:4173", tech: ["Flutter","Supabase","Python"], featured:false, completed:true }
+    {title: "Personal Website", description:"A workspace for displaying my projects, skills, and experience to the world!",image:"/personal_website.png", github_link:"https://github.com/WillDomine/personal-website", website_link: "https://willdomine.com", tech: ["Astro","Tailwind","Render"], featured:true, completed:true },
+    {title: "Aurelius", description:"A Vulkan game engine, created for optimization.",image:"", github_link:"https://github.com/WillDomine/Aurelius", tech: ["C++","Vulkan API"], featured:false, completed:false },
+    {title: "Scope of Employment", description:"Legal Chatbot, utilizing RAG to feed context to LLM",image:"", tech: ["Ollama","Qdrant","FastAPI","Flask"], featured:true, completed:true },
+    {title: "HoopHub", description:"NBA statistics tracker",image:"", github_link:"https://github.com/WillDomine/HoopHub", tech: ["Flutter","Supabase","Python"], featured:false, completed:true }
 ]
 
 export { projects }
