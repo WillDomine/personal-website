923a486dc85c73083725070c000c5ef8d93984a8
Will Domine <willdomine1@outlook.com>
2025-12-19 22:55:40 -0800

    completed home, projects, and skills

---
+++ main-astro/src/utils/projects.js
@@ -0,0 +1,6 @@
+const projects = [
+    {title: "Personal Website", description:"A workspace for displaying my projects, skills, and experience to the world!",image:"/personal_website.png", github_link:"https://localhost:4173", website_link: "https://loaclhost:4173", tech: ["Astro","Tailwind","Render"], featured:true, completed:false },
+    {title: "Aurelius", description:"A Vulkan game engine, created for optimization.",image:"", github_link:"https://localhost:4173", website_link: "https://loaclhost:4173", tech: ["C++","Vulkan API"], featured:true, completed:false }
+]
+
+export { projects }
---
+++ main-astro/src/utils/skills.js
@@ -0,0 +1,34 @@
+const categories = [
+  {
+    group: "Languages",
+    items: ["Java", "Python", "Go", "TypeScript", "Rust", "C#", "C++"],
+    comment: "// high_performance_engines"
+  },
+  {
+    group: "Frameworks",
+    items: ["Astro", "Spring Boot", "React.js", "Flutter", "FastAPI", ".NET", "Node.js", "Tauri"],
+    comment: "// application_architecture"
+  },
+  {
+    group: "AI_ML",
+    items: ["PyTorch", "TensorFlow", "Ollama", "Qdrant"],
+    comment: "// neural_processing"
+  },
+  {
+    group: "Cloud_DevOps",
+    items: ["Azure", "AWS", "Docker", "Kubernetes", "Git"],
+    comment: "// infrastructure_as_code"
+  },
+  {
+    group: "Data_Service",
+    items: ["Postgres", "MongoDB", "Redis", "Kafka", "GraphQL", "JWT"],
+    comment: "// persistent_storage"
+  },
+  {
+    group:"Operating_Systems",
+    items: ["Windows_11", "Ubuntu_LTS", "Arch_Linux", "MacOS"],
+    comment: "// kernel_interfaces_and_environments"
+  }
+];
+
+export { categories }
